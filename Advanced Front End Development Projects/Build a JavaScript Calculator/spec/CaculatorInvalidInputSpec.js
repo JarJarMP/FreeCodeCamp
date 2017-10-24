bf0fddf818bfe30ccae4b3869d35afc9e494f6e8
Peter Maroti <talk@marotipeter.com>
2017-10-24 14:29:23 +0200

    this what they call - a fresh start

--- Advanced Front End Development Projects/Build a JavaScript Calculator/spec/CaculatorInvalidInputSpec.js
+++
@@ -1,7 +0,0 @@
-var Calculator = require('../calculator');
-
-xdescribe("Calculator has to be able", function() {
-  it("to handle wrong input formats", function() {
-    var c = new Calculator();
-  });
-});
